bfab757340de8c85d06e54c1e1619d23fd960c29
ragayathri97 <ragayathri97@gmail.com>
2025-03-02 23:52:11 +0530

    install react-router-dom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,31 @@ import youtube_icon from '../../assets/youtube_icon.png'
 import twitter_icon from '../../assets/twitter_icon.png'
 import instagram_icon from '../../assets/instagram_icon.png'
 import facebook_icon from '../../assets/facebook_icon.png'
+
 const Footer = () => {
   return (
     <div className='footer'>
       <div class="footer-icons">
         <img src={facebook_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={youtube_icon} alt="" />
       </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investore Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact US</li>
+      </ul>
+      <p className='copyright-text'>@ 1997-20025 Netflix, Inc.</p>
     </div>
   )
 }
