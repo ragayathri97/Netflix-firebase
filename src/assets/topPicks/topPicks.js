6cb51ac704aa630542c8a2b55b7435ce01678f9f
ragayathri97 <ragayathri97@gmail.com>
2025-03-04 10:55:28 +0530

    img

--- src/assets/topPicks/topPicks.js
+++ src/assets/topPicks/topPicks.js
@@ -7,11 +7,11 @@ import tp6 from './tp6.jpg';
 import tp7 from './tp7.jpg';
 import tp8 from './tp8.jpg';
 import tp9 from './tp9.jpg';
-// import tp10 from './tp10.jpg';
-// import tp11 from './tp11.jpg';
-// import tp12 from './tp12.jpg';
-// import tp13 from './tp13.jpg';
-// import tp14 from './tp14.jpg';
+import tp10 from './tp10.jpg';
+import tp11 from './tp11.jpg';
+import tp12 from './tp12.jpg';
+import tp13 from './tp13.jpg';
+import tp14 from './tp14.jpg'; 
 
 const topPicks = [
   { image: tp1, name: 'The Shawshank Redemption' },
@@ -23,11 +23,11 @@ const topPicks = [
   { image: tp7, name: 'The Lord of the Rings: The Return of the King' },
   { image: tp8, name: 'Forrest Gump' },
   { image: tp9, name: 'Inception' },
-//   { image: tp10, name: 'Fight Club' },
-//   { image: tp11, name: 'The Matrix' },
-//   { image: tp12, name: 'Goodfellas' },
-//   { image: tp13, name: 'Se7en' },
-//   { image: tp14, name: 'Interstellar' },
+  { image: tp10, name: 'Fight Club' },
+  { image: tp11, name: 'The Matrix' },
+  { image: tp12, name: 'Goodfellas' },
+  { image: tp13, name: 'Se7en' },
+  { image: tp14, name: 'Interstellar' },
 ];
 
 
--- src/assets/upcoming/upcomingMovies.js
+++ src/assets/upcoming/upcomingMovies.js
@@ -6,12 +6,12 @@ import up5 from './up5.jpg';
 import up6 from './up6.jpg';
 import up7 from './up7.jpg';
 import up8 from './up8.jpg';
-// import up9 from './up9.jpg';
-// import up10 from './up10.jpg';
-// import up11 from './up11.jpg';
-// import up12 from './up12.jpg';
-// import up13 from './up13.jpg';
-// import up14 from './up14.jpg';
+import up9 from './up9.jpg';
+import up10 from './up10.jpg';
+import up11 from './up11.jpg';
+import up12 from './up12.jpg';
+import up13 from './up13.jpg';
+import up14 from './up14.jpg';
 
 const upcomingMovies = [
   { image: up1, name: 'Dune: Part Two' },
@@ -22,12 +22,12 @@ const upcomingMovies = [
   { image: up6, name: 'Wonka' },
   { image: up7, name: 'Napoleon' },
   { image: up8, name: 'Rebel Moon' },
-  // { image: up9, name: 'The Color Purple' },
-  // { image: up10, name: 'Kraven the Hunter' },
-  // { image: up11, name: 'Madame Web' },
-  // { image: up12, name: 'Argylle' },
-  // { image: up13, name: 'Drive-Away Dolls' },
-  // { image: up14, name: 'Mickey 17' },
+  { image: up9, name: 'The Color Purple' },
+  { image: up10, name: 'Kraven the Hunter' },
+  { image: up11, name: 'Madame Web' },
+  { image: up12, name: 'Argylle' },
+  { image: up13, name: 'Drive-Away Dolls' },
+  { image: up14, name: 'Mickey 17' },
 ];
 
 export default upcomingMovies;
