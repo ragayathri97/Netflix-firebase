b92f55b70b28c96cbec23bcf9966174d9c7de774
ragayathri97 <ragayathri97@gmail.com>
2025-03-04 09:35:34 +0530

    pic

--- src/assets/blockbuster/blockbusterMovies.js
+++ src/assets/blockbuster/blockbusterMovies.js
@@ -2,10 +2,10 @@ import bb1 from './bb1.jpg';
 import bb2 from './bb2.jpg';
 import bb3 from './bb3.jpg';
 import bb4 from './bb4.jpg';
-// import bb5 from './bb5.jpg';
-// import bb6 from './bb6.jpg';
-// import bb7 from './bb7.jpg';
-// import bb8 from './bb8.jpg';
+import bb5 from './bb5.jpg';
+import bb6 from './bb6.jpg';
+import bb7 from './bb7.jpg';
+import bb8 from './bb8.jpg';
 // import bb9 from './bb9.jpg';
 // import bb10 from './bb10.jpg';
 // import bb11 from './bb11.jpg';
@@ -18,10 +18,10 @@ const blockbusterMovies = [
   { image: bb2, name: 'Avatar: The Way of Water' },
   { image: bb3, name: 'Top Gun: Maverick' },
   { image: bb4, name: 'Spider-Man: No Way Home' },
-//   { image: bb5, name: 'Jurassic World Dominion' },
-//   { image: bb6, name: 'Doctor Strange in the Multiverse of Madness' },
-//   { image: bb7, name: 'The Batman' },
-//   { image: bb8, name: 'Black Panther: Wakanda Forever' },
+  { image: bb5, name: 'Jurassic World Dominion' },
+  { image: bb6, name: 'Doctor Strange in the Multiverse of Madness' },
+  { image: bb7, name: 'The Batman' },
+  { image: bb8, name: 'Black Panther: Wakanda Forever' },
 //   { image: bb9, name: 'Minions: The Rise of Gru' },
 //   { image: bb10, name: 'Thor: Love and Thunder' },
 //   { image: bb11, name: 'Fantastic Beasts: The Secrets of Dumbledore' },
--- src/assets/upcoming/upcomingMovies.js
+++ src/assets/upcoming/upcomingMovies.js
@@ -1,11 +1,11 @@
 import up1 from './up1.jpg';
 import up2 from './up2.jpg';
-// import up3 from './up3.jpg';
-// import up4 from './up4.jpg';
-// import up5 from './up5.jpg';
-// import up6 from './up6.jpg';
-// import up7 from './up7.jpg';
-// import up8 from './up8.jpg';
+import up3 from './up3.jpg';
+import up4 from './up4.jpg';
+import up5 from './up5.jpg';
+import up6 from './up6.jpg';
+import up7 from './up7.jpg';
+import up8 from './up8.jpg';
 // import up9 from './up9.jpg';
 // import up10 from './up10.jpg';
 // import up11 from './up11.jpg';
@@ -16,12 +16,12 @@ import up2 from './up2.jpg';
 const upcomingMovies = [
   { image: up1, name: 'Dune: Part Two' },
   { image: up2, name: 'Oppenheimer' },
-  // { image: up3, name: 'Killers of the Flower Moon' },
-  // { image: up4, name: 'The Marvels' },
-  // { image: up5, name: 'Aquaman and the Lost Kingdom' },
-  // { image: up6, name: 'Wonka' },
-  // { image: up7, name: 'Napoleon' },
-  // { image: up8, name: 'Rebel Moon' },
+  { image: up3, name: 'Killers of the Flower Moon' },
+  { image: up4, name: 'The Marvels' },
+  { image: up5, name: 'Aquaman and the Lost Kingdom' },
+  { image: up6, name: 'Wonka' },
+  { image: up7, name: 'Napoleon' },
+  { image: up8, name: 'Rebel Moon' },
   // { image: up9, name: 'The Color Purple' },
   // { image: up10, name: 'Kraven the Hunter' },
   // { image: up11, name: 'Madame Web' },
